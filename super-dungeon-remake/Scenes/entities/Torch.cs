3582f3ebccf9189861527862690aa4e5397b5f9f
LGZ <oppo0310@qq.com>
2025-08-02 09:27:09 +0800

    fix(Torch): 修正动画精灵节点名称并调整初始帧进度
    修复动画精灵节点名称从"AnimatedSprite"到"AnimatedSprite2D"的引用错误
    调整初始帧进度使多个火炬动画不同步

--- super-dungeon-remake/Scenes/entities/Torch.cs
+++ super-dungeon-remake/Scenes/entities/Torch.cs
@@ -6,7 +6,7 @@ public partial class Torch : Node2D
     public override void _Ready()
     {
         // Stop all torches moving in sync
-        var animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite");
+        var animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
         if (animatedSprite != null)
         {
             animatedSprite.Frame = GD.RandRange(0, 4);
